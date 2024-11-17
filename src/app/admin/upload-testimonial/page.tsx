f0f6cdf7fe78467e791e74dfa003245153740e86
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-11-17 18:39:39 +0545

    controller folder only removed

---
+++ src/app/admin/upload-testimonial/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { decodeToken } from "@/components/utils/decodeToken.js";
+import Navbar from "@/admin-components/Navbar/Navbar";
+
+interface DecodedToken {
+  username: string;
+  role: string;
+  userId: string;
+}
+
+const Page = () => {
+  // const pathname = usePathname();
+  const [expiryTime, setExpiryTime] = useState(0);
+  const [isUserId, setIsUserId] = useState("");
+  // const [isUserIdJwt, setIsUserIdJwt] = useState("");
+  const [isDecodedToken, setIsDecodedToken] = useState<DecodedToken | null>(
+    null
+  );
+  // const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    const isFirstRender = localStorage.getItem("firstRender");
+    if (isFirstRender) {
+      // Refresh the page
+      window.location.reload();
+
+      // Remove the flag to prevent future refreshes
+      localStorage.removeItem("firstRender");
+    }
+    // setCurrentPath(pathname.split("/").slice(-1)[0]);
+  }, []);
+  const router = useRouter();
+
+  // Check if the user is authenticated
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.push("/");
+    } else {
+      try {
+        const decodedToken = decodeToken(token);
+        setIsDecodedToken(decodedToken);
+        // setIsUserIdJwt(decodedToken.userId);
+        if (decodedToken && decodedToken.exp) {
+          setExpiryTime(decodedToken.exp);
+        }
+        if (decodedToken && decodedToken.username && decodedToken.userId) {
+          // Redirect to the URL format with query params if not already there
+          const queryParams = new URLSearchParams(window.location.search);
+          const u_id = queryParams.get("Id") || "";
+          setIsUserId(u_id);
+          // Check if the query parameters already exist in the URL
+
+          const urlUsername = queryParams.get("username") || "";
+          const urlRole = queryParams.get("role") || "";
+          const urlId = queryParams.get("Id") || "";
+          if (
+            !queryParams.has("username") ||
+            !queryParams.has("role") ||
+            !queryParams.has("Id") ||
+            urlUsername !== decodedToken.username ||
+            urlRole !== decodedToken.role ||
+            urlId !== decodedToken.userId
+          ) {
+            router.push(
+              `/admin/upload-testimonial?username=${decodedToken.username}&role=${decodedToken.role}&Id=${decodedToken.userId}`
+            );
+          }
+        }
+      } catch (error) {
+        console.error("Error decoding token:", error);
+        console.error("Token:", isDecodedToken)
+        // In case of an invalid token, redirect to login
+        router.push("/");
+      }
+    }
+  }, [router, isUserId]);
+
+  // Check if the token has expired
+  useEffect(() => {
+    if (expiryTime > 0) {
+      const currentTime = Date.now() / 1000;
+
+      if (expiryTime < currentTime) {
+        // If token is expired, redirect to login
+        localStorage.removeItem("token");
+        router.push("/");
+      }
+    }
+  }, [expiryTime, router]);
+
+  return (
+    <>
+      <Navbar />
+      <p>Create form to upload testimonial</p>
+      <p>Also below form, there should be all testimonial listed and admin can delete particular testimonial</p>
+      <p>Does edit necessary in testimonial? Think....</p>
+    </>
+  );
+};
+
+export default Page;
