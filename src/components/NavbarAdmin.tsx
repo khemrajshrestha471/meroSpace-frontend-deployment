cc0c399b5639bb0e88cd21b0337c9656f45f1fe8
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-11-17 21:01:11 +0545

    path updated

--- src/components/NavbarAdmin.tsx
+++ src/components/NavbarAdmin.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { decodeToken } from "@/components/utils/decodeToken.js";
 import { useEffect, useState } from "react";
